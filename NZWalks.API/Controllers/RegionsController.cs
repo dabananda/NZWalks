1d8a5788a925d523434c5afd09503e8f2a838281
dabananda <dabananda.dev@gmail.com>
2025-07-19 16:47:05 +0600

    Implemented GET & GET by Id method

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        public readonly NZWalksDbContext _dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}
