b6afd66a61cbe4ac2ab2a93e16cc6ecff9b159d0
dabananda <dabananda.dev@gmail.com>
2026-03-19 21:46:04 +0600

    Introduce repository pattern for Region data access

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly IRegionRepository _regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            _regionRepository = regionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = _regionRepository.GetRegions();
+           
+            var regionsDto = new List<Region>();
+            regions.ToList().ForEach(region =>
+            {
+                var regionDto = new Region()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                };
+
+                regionsDto.Add(regionDto);
+            });
+
+            return Ok(regionsDto);
+        }
+    }
+}
---
+++ NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext _nZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            _nZWalksDbContext = nZWalksDbContext;
+        }
+
+        public IEnumerable<Region> GetRegions()
+        {
+           return _nZWalksDbContext.Regions.ToList();
+        }
+    }
+}
